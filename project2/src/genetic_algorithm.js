7752ba5388db61e50bf9f4704c267b8ef710364a
Roy <roydhowie@gmail.com>
2017-02-25 18:33:10 -0500

    basic idea of genetic algorithm applied to images

---
+++ project2/src/genetic_algorithm.js
@@ -0,0 +1,122 @@
+let Individual = require('./individual.js').Individual
+
+class GeneticAlgorithm {
+  constructor (max_pop_size, chromo_len=50, goal, width, height) {
+    this.max_pop_size = max_pop_size
+    this.chromo_len = chromo_len
+    this.goal = goal
+    this.width = width
+    this.height = height
+
+    // Create an initial, random population of `max_pop_size` individuals.
+    this.population = []
+    for (let i = 0; i < this.max_pop_size; i++) {
+      this.population[i] = new Individual(this.chromo_len, width, height)
+    }
+
+    // Record the number of times the `GeneticAlgorithm.fitness` is called; this
+    // is an alternative way of counting the number of individuals encountered
+    // throughout the algorithm's lifecycle.
+    this.fitness_calls = 0
+  }
+
+  // Returns a fitness value between 1 (bad) and 100 (great)
+  fitness (individual) {
+    this.fitness_calls += 1
+    return individual.fitness(this.goal)
+  }
+
+  play (should_continue) {
+    console.log('gen,avg_fit,max_fit')
+    let generation = 1
+    do {
+      let cumulative_sum = [0]
+      let sum = 0.0
+      let max_fitness = -Infinity
+
+      for (let i = 0; i < this.population.length; i++) {
+        // Record the fitness of invidual #i
+        let indiv_fit = this.fitness(this.population[i])
+
+        // Keep track of the max fitness of this population
+        max_fitness = Math.max(max_fitness, indiv_fit)
+
+        // Keep a running total of the sum of the fitness values. This will be
+        // used to randomly select individuals
+        sum += indiv_fit
+        // cumulative_sum[i] = sum
+        cumulative_sum[i+1] = sum
+      }
+
+      // Average fitness value of population [for data purposes].
+      let average = sum / (cumulative_sum.length - 1)
+
+      let next_pop = []
+      for (let i = 0; i < this.population.length; i++) {
+        let rand_fit_val = Math.random() * sum
+        let random_index = GeneticAlgorithm.search(cumulative_sum, rand_fit_val)
+
+        // clone the individual at `random_index-1` because GeneticAlgorithm.
+        // search will return an index between 0 and pop_size inclusive.
+        next_pop.push(this.population[random_index].clone())
+      }
+
+      // Mate individuals
+      for (let i = 0; i < next_pop.length; i = i + 2) {
+        let mother = next_pop[i]
+        let father = next_pop[i + 1]
+
+        let [ daughter, son ] = mother.mate(father)
+
+        next_pop[i] = daughter
+        next_pop[i + 1] = son
+      }
+
+      // Possible mutate individuals
+      for (let i = 0; i < next_pop.length; i++) {
+        next_pop[i].attemptMutation()
+      }
+
+      // Destroy the old population (for memory purposes) and store the next
+      // generation.
+      this.population.forEach(individual => {
+        individual.paint()
+        individual.destroy()
+      })
+      this.population = next_pop
+
+      console.log(`${generation},${average},${max_fitness}`)
+      generation += 1
+    } while (should_continue(this))
+  }
+
+  // Search for the last element which is less than `goal` and return its index.
+  // For example, if weights := [1, 2, 3, 4, 5] and our array of cumulative sums
+  // is array := [0, 1, 3, 6, 10, 15], then
+  //    search(array, 0)    -> 0
+  //    search(array, 1)    -> 1
+  //    search(array, 3.5)  -> 2
+  //    search(array, 12)   -> 4
+  //    search(array, 15)   -> 4
+  //    search(array, 9000) -> 5
+  // Works for input not in [0, sum(array)], but is only intended to be used for
+  // non-negative `goal`.
+  static search (arr, goal) {
+    let low = 0
+    let high = arr.length - 1
+
+    while (low != high) {
+      let mid = Math.round((low + high) / 2)
+
+      if (goal <= arr[mid]) {
+        high = mid - 1
+      } else {
+        low = mid
+      }
+    }
+
+    return low
+  }
+}
+
+module.exports = { GeneticAlgorithm }
