91760b463d66260bd0d47ad8e70627b2ce8b9f88
Roy <roydhowie@gmail.com>
2017-02-26 18:02:58 -0500

    updating testing program

--- project2/src/test.js
+++ project2/src/test.js
@@ -1,26 +1,43 @@
 let GA = require('./genetic_algorithm.js').GeneticAlgorithm
 let MUT_CHANCE = { individual: 100, gene: 25 }
-
-let logger = console.log.bind(console)
+const TRIALS = 500
 
 // hold console.log hostage for testing
+let logger = console.log.bind(console)
 console.log = () => {}
 
 let keep_playing = () => {
   let i = 0
-  return (instance) => i++ < 100000 && instance.max_fitness < 23.9
+  let last_fitness = 0
+  let gap = 0
+  return (instance) => {
+    if (last_fitness === instance.max_fitness) {
+      return ++gap < 1000
+    } else {
+      gap = 0
+    }
+    last_fitness = instance.max_fitness
+    return i++ < 100000 && instance.max_fitness < 23.97
+  }
 }
 
-logger('pop_size,generations,calls_of_f(x)')
-for (let i = 1; i < 26; i++) {
+logger('pop_size,generations,calls_of_f(x),average_max_fitness')
+
+for (let i = 1; i < 41; i++) {
   let population = 6 * i
   let generations = 0.0
   let calls = 0.0
-  for (let j = 0; j < 250; j++) {
+  let fitness = 0.0
+
+  for (let j = 0; j < TRIALS; j++) {
     let ga = new GA(population, 20, MUT_CHANCE)
     ga.play(keep_playing())
+
+    fitness += ga.max_fitness
     generations += ga.generation
     calls += ga.fitness_calls
   }
-  logger(`${population},${generations/100},${calls/100}`)
+
+  logger([ population, generations / TRIALS, calls / TRIALS,
+    fitness / TRIALS ].join(','))
 }
