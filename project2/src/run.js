4a0704840a819e7b405658606504eb93781cb09d
Roy <roydhowie@gmail.com>
2017-02-26 23:13:46 -0500

    run.js should match termination criterion from test.js

--- project2/src/run.js
+++ project2/src/run.js
@@ -1,3 +1,3 @@
 let GA = require('./genetic_algorithm.js').GeneticAlgorithm
 
-new GA(50, 20).play((instance) => instance.max_fitness < 23.975)
+new GA(50, 20).play((instance) => instance.max_fitness < 23.97)
